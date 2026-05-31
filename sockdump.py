rafaelgieschke/sockdump

commit 8a78f1783a03d492c6f2742bc9d71a164dc90518

Remove deprecated _pack field
Currently, this causes the following deprecation warning on Python 3.14:

Due to '_pack_', the 'Packet' Structure will use memory layout compatible with MSVC (Windows).
If this is intended, set _layout_ to 'ms'. The implicit default is deprecated and slated to become an error in Python 3.19.

sockdump.py
@@ -234,7 +234,6 @@ def build_filter(sock_path):
     return filter, path_len, path_len_u64
 
 class Packet(ct.Structure):
-    _pack_ = 1
     _fields_ = [
         ('pid', ct.c_uint),
         ('peer_pid', ct.c_uint),

commit 8f3d74e861b09a195ff694468d834337713ee5f6

fixup! Remove deprecated _pack field
This broke `event += PACKET_SIZE` if `struct packet`'s size is not a multiple
of its largest member's size.

Fixes: 8a78f1783a03d492c6f2742bc9d71a164dc90518

sockdump.py
@@ -257,13 +257,14 @@ class Packet(ct.Structure):
         ('peer_inode', ct.c_uint64),
         ('comm', ct.c_char * TASK_COMM_LEN),
         ('path', ct.c_char * UNIX_PATH_MAX),
+        ('data', ct.c_char * 0),
         # variable length data
     ]
 
 PCAP_LINK_TYPE = 228    # IPV4
 # PCAP_LINK_TYPE = 229    # IPV6
 
-PACKET_SIZE = ct.sizeof(Packet)
+PACKET_SIZE = Packet.data.byte_offset
 
 packet_count = 0
 flush_after_each_packet = False